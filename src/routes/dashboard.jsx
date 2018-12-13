67c2dfe5c4f1a02e3440239a550c695a1cfc2b55
Tomas Zaicevas <tozaicevas@gmail.com>
2018-12-14 00:48:50 +0200

    /cars, /people, Home

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -18,8 +18,8 @@ import Accessibility from "@material-ui/icons/Accessibility";
 const dashboardRoutes = [
   {
     path: "/dashboard",
-    sidebarName: "Dashboard",
-    navbarName: "Material Dashboard",
+    sidebarName: "Home",
+    navbarName: "Epicenter Home",
     icon: Dashboard,
     component: DashboardPage
   },
@@ -31,14 +31,14 @@ const dashboardRoutes = [
     component: UserProfile
   },
   {
-    path: "/cartable",
+    path: "/cars",
     sidebarName: "Found Cars",
     navbarName: "Found Cars",
     icon: "directions_car",
     component: CarTableList
   },
   {
-    path: "/peopletable",
+    path: "/people",
     sidebarName: "Found People",
     navbarName: "Found People",
     icon: Accessibility,
