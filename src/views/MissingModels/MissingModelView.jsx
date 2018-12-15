44be8fcef95e7cd7d783cb552270bbb9f9515b36
Valdas Rakutis <valdas@rakutis.lt>
2018-12-16 01:51:57 +0200

    Fix some stuff here and there, add a loading message/last update message

--- src/views/MissingModels/MissingModelView.jsx
+++ src/views/MissingModels/MissingModelView.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import GridContainer from "components/Grid/GridContainer.jsx";
 import TableList from "components/CustomTables/TableList.jsx";
+import DeleteItemAlert from "../../components/CustomAlerts/DeleteItemAltert";
+import { Dialog } from "@material-ui/core";
 
 class MissingModelView extends React.Component {
   render() {
@@ -13,6 +15,7 @@ class MissingModelView extends React.Component {
           url={"https://epicentereu.azurewebsites.net/api/missing/persons/"}
           responseLenghtCallback={count => {}}
           last24hrsCallback={count => {}}
+          handleDelete={this.handleDelete}
           limit={-1}
           color={"success"}
           http={"GET"}
@@ -24,6 +27,7 @@ class MissingModelView extends React.Component {
           url={"https://epicentereu.azurewebsites.net/api/missing/cars/"}
           responseLenghtCallback={count => {}}
           last24hrsCallback={count => {}}
+          handleDelete={this.handleDelete}
           limit={-1}
           color={"warning"}
           http={"GET"}
